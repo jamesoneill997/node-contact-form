688f5313d201fd60f758fb8099a557e9ce42a63f
jamesoneill997 <jamesoneill997@gmail.com>
2020-04-24 05:05:34 +0100

    more user friendly front-end errors

--- src/public/utils/sendEmail.js
+++ src/public/utils/sendEmail.js
@@ -6,19 +6,24 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 
 const sendMail = async (name, email, phone, subject, message) => {
-    if(validator.isEmail(email)){
+    try {
         const msg = {
             to: yourEmail,
             from: email,
             subject,
             text: message,
         }
-    
-    await sgMail.send(msg)
-    }
 
-    else{
-        throw new Error('Email is invalid')
+        await sgMail.send(msg)
+
+    } catch (ResponseError) {
+        if(!validator.isEmail(email)){
+            throw new Error('Email is invalid')
+        }
+        else{
+            throw new Error('Internal server error. Please contact ' + yourEmail + ' directly')
+        }
+        
     }
 }
 
